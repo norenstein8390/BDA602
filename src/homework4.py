aaeea42d5c4c76a1d1d4b89a66f0648880462ee5
Nathan Orenstein <norenstein8390@sdsu.edu>
2022-10-12 22:00:22 -0700

    Homework 4 Update - Wednesday Oct 12

--- src/homework4.py
+++ src/homework4.py
@@ -6,6 +6,8 @@ import statsmodels.api as sm
 from plotly import express as px
 from plotly import figure_factory as ff
 from plotly import graph_objects as go
+from plotly.subplots import make_subplots
+from sklearn.ensemble import RandomForestClassifier
 
 
 def response_type_check(df, response):
@@ -202,34 +204,54 @@ def linear_regression(df, predictor_name, response):
     fig.show()
 
 
-def diff_with_mean_of_resp(df, predictor_name, response_name):
-    print(df)
-    num_bins = 10
+def diff_with_mean_of_resp(df, predictor_name, response_name, cat_check, bool_check):
+    if bool_check is True:
+        unique_responses = df[response_name].unique()
+        df.loc[df[response_name] == unique_responses[0], response_name] = 0
+        df.loc[df[response_name] == unique_responses[1], response_name] = 1
+
     predictor = df[predictor_name]
     response = df[response_name]
-    min_value = min(predictor)
-    max_value = max(predictor)
-    full_width = abs(max_value - min_value)
-    bin_width = full_width / num_bins
-    lower_bins = np.arange(start=min_value, stop=max_value, step=bin_width)
-    upper_bins = np.arange(start=min_value + bin_width, stop=max_value, step=bin_width)
-    """
-    bin_centers = np.arange(
-        start=min_value + (bin_width / 2), stop=max_value, step=bin_width
-    )
-    """
+
+    bin_means = []
+    bin_mean_minus_pop_mean = []
+    mean_squared_diff = []
+    weighted_mean_squared_diff = []
+
+    if cat_check is True:
+        num_bins = df[predictor_name].nunique()
+        possible_cat_predictors = df[predictor_name].unique()
+    else:
+        num_bins = 10
 
     bin_counts = np.repeat(0, num_bins)
     bin_responses = np.repeat(0, num_bins)
-    bin_means = []
-    mean_squared_diff = []
+
+    if cat_check is False:
+        min_value = min(predictor)
+        max_value = max(predictor)
+        full_width = abs(max_value - min_value)
+        bin_width = full_width / num_bins
+        lower_bins = np.arange(start=min_value, stop=max_value, step=bin_width)
+        upper_bins = np.arange(
+            start=min_value + bin_width, stop=max_value + bin_width, step=bin_width
+        )
+        bin_centers = np.arange(
+            start=min_value + (bin_width / 2), stop=max_value, step=bin_width
+        )
+        bin_counts = np.repeat(0, num_bins)
+        bin_responses = np.repeat(0, num_bins)
 
     for i in range(len(predictor)):
         cur_val = predictor[i]
         cur_response = response[i]
 
         for bin_num in range(num_bins):
-            if cur_val >= lower_bins[bin_num] and cur_val <= upper_bins[bin_num]:
+            if (cat_check is True and cur_val == possible_cat_predictors[bin_num]) or (
+                cat_check is False
+                and cur_val >= lower_bins[bin_num]
+                and cur_val <= upper_bins[bin_num]
+            ):
                 bin_counts[bin_num] += 1
                 bin_responses[bin_num] += cur_response
                 break
@@ -239,9 +261,103 @@ def diff_with_mean_of_resp(df, predictor_name, response_name):
     for i in range(num_bins):
         bin_mean = bin_responses[i] / bin_counts[i]
         bin_means.append(bin_mean)
+        bin_mean_minus_pop_mean.append(bin_mean - population_mean)
         mean_squared_diff.append((bin_mean - population_mean) ** 2)
 
-    px.histogram(df, x=predictor_name, bins=num_bins)
+        weight = bin_counts[i] / len(predictor)
+        weighted_mean_squared_diff.append(
+            (weight * ((bin_mean - population_mean) ** 2))
+        )
+
+    bin_mean_minus_pop_mean = np.array(bin_mean_minus_pop_mean)
+
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    if cat_check is True:
+        fig.add_trace(
+            go.Bar(x=possible_cat_predictors, y=bin_counts, name="Population"),
+            secondary_y=False,
+        )
+
+        possible_cat_predictors.sort()
+
+        fig.add_trace(
+            go.Scatter(
+                x=possible_cat_predictors,
+                y=bin_mean_minus_pop_mean,
+                name="\u03BC(i) - \u03BC(population)",
+            ),
+            secondary_y=True,
+        )
+    else:
+        fig.add_trace(
+            go.Bar(x=bin_centers, y=bin_counts, name="Population"), secondary_y=False
+        )
+
+        fig.add_trace(
+            go.Scatter(
+                x=bin_centers,
+                y=bin_mean_minus_pop_mean,
+                name="\u03BC(i) - \u03BC(population)",
+            ),
+            secondary_y=True,
+        )
+
+    fig.add_hline(y=population_mean, name="population mean")
+
+    fig.update_layout(
+        title_text="Difference with Mean of Response - Predictor {}".format(
+            predictor_name
+        )
+    )
+
+    fig.update_xaxes(title_text="Predictor Bin")
+    fig.update_yaxes(title_text="Response", secondary_y=False)
+    fig.update_yaxes(
+        title_text="Population",
+        secondary_y=True,
+        range=[-(max(response)), max(response)],
+    )
+    fig.update_layout(
+        dict(
+            yaxis2={"anchor": "x", "overlaying": "y", "side": "left"},
+            yaxis={"anchor": "x", "domain": [0.0, 1.0], "side": "right"},
+        )
+    )
+    fig.show()
+
+    dif_w_mean_of_resp = np.mean(mean_squared_diff)
+    print(
+        "Difference with mean of response (unweighted) = {}".format(
+            str(dif_w_mean_of_resp)
+        )
+    )
+    print(
+        "Difference with mean of response (weighted) = {}".format(
+            str(sum(weighted_mean_squared_diff))
+        )
+    )
+
+
+def random_forest(df, cont_list, response):
+    cont_list.append(response)
+    pd.set_option("mode.use_inf_as_na", True)
+
+    full_list = df[cont_list]
+    full_list = full_list.dropna()
+
+    X = full_list[cont_list]
+    X = X.drop(response, axis=1)
+    y = full_list[response]
+    cont_list = cont_list.remove(response)
+
+    classifier = RandomForestClassifier()
+    classifier.fit(X, y)
+
+    feature_imp = pd.Series(
+        classifier.feature_importances_, index=cont_list
+    ).sort_values(ascending=False)
+    print(feature_imp)
 
 
 def main():
@@ -262,19 +378,24 @@ def main():
     response = "survived"
 
     # Determine if response is continuous or boolean
-    boolean_check = response_type_check(df, response)
+    bool_check = response_type_check(df, response)
+
+    cont_list = []  # will store continuous predictors for random forest
 
     # Loop through each predictor column
     for predictor in predictors:
         # Determine if the predictor is cat/cont
         cat_check = cat_cont_check(df, predictor)
 
+        if cat_check is False:
+            cont_list.append(predictor)
+
         # Automatically generate the necessary plot(s) to inspect it
-        if boolean_check is True and cat_check is True:
+        if bool_check is True and cat_check is True:
             bool_response_cat_predictor_plots(df, predictor, response)
-        elif boolean_check is True and cat_check is False:
+        elif bool_check is True and cat_check is False:
             bool_response_cont_predictor_plots(df, predictor, response)
-        elif boolean_check is False and cat_check is True:
+        elif bool_check is False and cat_check is True:
             cont_response_cat_predictor_plots(df, predictor, response)
         else:
             cont_response_cont_predictor_plots(df, predictor, response)
@@ -284,13 +405,16 @@ def main():
         # Regression: Continuous response
         # Logistic regression: Boolean response
         if cat_check is False:
-            if boolean_check is True:
+            if bool_check is True:
                 logistic_regression(df, predictor, response)
             else:
                 linear_regression(df, predictor, response)
 
         # Difference with mean of response along with it's plot (weighted and unweighted)
-        diff_with_mean_of_resp(df, response, predictor)
+        diff_with_mean_of_resp(df, predictor, response, cat_check, bool_check)
+
+    # Random Forest Variable importance ranking (continuous predictors only)
+    random_forest(df, cont_list, response)
 
 
 if __name__ == "__main__":

3819228f01a207f5c0ee28b862cace2113a65beb
Nathan Orenstein <norenstein8390@sdsu.edu>
2022-11-11 12:04:32 -0800

    Friday - Nov 11 Update (Basically Restarted)

--- src/homework5.sql
+++ src/homework5.sql
@@ -1,96 +1,102 @@
 USE baseball;
 
-DROP TABLE IF EXISTS batter_game_team_stats;
+DROP TABLE IF EXISTS away_team_batter_stats;
 
-CREATE TABLE batter_game_team_stats AS
-SELECT
-    bc.game_id, -- need to use bc because of 'double' issue mentioned below
-    bc.team_id,
-    SUM(bc.atBat) AS AB,
-    SUM(bc.Hit) AS H,
-    SUM(bc.Single) AS singles,
-    SUM(bc.Double) AS doubles, -- making these plural because 'double' is a key word
-    SUM(bc.Triple) AS triples,
-    SUM(bc.Home_Run) AS HR,
-    SUM(bc.Hit_By_Pitch) AS HBP,
-    SUM(bc.Walk) AS BB,
-    SUM(bc.Sac_fly) AS SF,
-    SUM(bc.Strikeout) AS K
-FROM batter_counts bc
-GROUP BY game_id, team_id;
-
-DROP TABLE IF EXISTS starting_pitcher_game_team_stats;
-
-CREATE TABLE starting_pitcher_game_stats AS
+CREATE TABLE away_team_batter_stats AS
 SELECT
     game_id,
     team_id,
-    (outsPlayed / 3) AS IP,
+    atBat AS AB,
+    Hit AS H,
+    Single AS singles, -- using plural because 'Double' is a key word
+    team_batting_counts.Double AS doubles, -- have to use team_batting_counts because 'Double' is a key word
+    Triple AS triples,
+    Home_Run AS HR,
+    Hit_By_Pitch AS HBP,
+    Sac_Fly AS SF,
+    Walk AS BB
+FROM team_batting_counts
+WHERE homeTeam = 0;
+
+DROP TABLE IF EXISTS home_team_batter_stats;
+
+CREATE TABLE home_team_batter_stats AS
+SELECT
+    game_id,
+    team_id,
+    atBat AS AB,
+    Hit AS H,
+    Single AS singles, -- using plural because 'Double' is a key word
+    team_batting_counts.Double AS doubles, -- have to use team_batting_counts because 'Double' is a key word
+    Triple AS triples,
+    Home_Run AS HR,
+    Hit_By_Pitch AS HBP,
+    Sac_Fly AS SF,
+    Walk AS BB
+FROM team_batting_counts
+WHERE homeTeam = 1;
+
+DROP TABLE IF EXISTS away_starting_pitcher_stats;
+
+CREATE TABLE away_starting_pitcher_stats AS
+SELECT
+    game_id,
+    team_id,
+    outsPlayed / 3 AS IP,
     Walk AS BB,
     Hit AS H,
     atBat AS AB,
-    Strikeout AS K,
-    Home_Run AS HR
+    Strikeout AS K
 FROM pitcher_counts
-WHERE startingPitcher = 1;
+WHERE startingPitcher = 1
+AND homeTeam = 0;
 
-DROP TABLE IF EXISTS ten_feature_stats;
+DROP TABLE IF EXISTS home_starting_pitcher_stats;
 
-CREATE TABLE ten_feature_stats AS
+CREATE TABLE home_starting_pitcher_stats AS
 SELECT
-    b.game_id,
-    b.team_id,
-    b.H / b.AB AS team_avg,
-    (b.H + b.BB + b.HBP) / (b.AB + b.BB + b.HBP + b.SF) AS team_obp,
-    (b.singles + (2 * b.doubles) + (3 * b.triples) + (4 * b.HR)) / b.AB AS team_slg,
-    (b.H + b.BB + b.HBP) / (b.AB + b.BB + b.HBP + b.SF) + (b.singles + (2 * b.doubles) + (3 * b.triples) + (4 * b.HR)) / b.AB AS team_ops,
-    b.HR AS team_hr,
-    b.K AS team_hitters_k,
-    (p.BB + p.H) / NULLIF(p.IP, 0) AS sp_whip,
-    p.H / NULLIF(p.AB, 0) sp_baa,
-    p.K AS sp_k,
-    p.HR AS sp_hr
-FROM batter_game_team_stats b
-JOIN starting_pitcher_game_team_stats p
-ON b.game_id = p.game_id
-AND b.team_id = p.team_id;
+    game_id,
+    team_id,
+    outsPlayed / 3 AS IP,
+    Walk AS BB,
+    Hit AS H,
+    atBat AS AB,
+    Strikeout AS K
+FROM pitcher_counts
+WHERE startingPitcher = 1
+AND homeTeam = 1;
 
-DROP TABLE IF EXISTS game_results;
+DROP TABLE IF EXISTS hw5_feature_stats;
 
-CREATE TABLE game_results AS
+CREATE TABLE hw5_feature_stats AS
 SELECT
-    r.game_id,
-    r.team_id,
-    r.home_away,
-    b.winner_home_or_away
-FROM team_results r
-JOIN boxscore b
-ON r.game_id = b.game_id;
-
-DROP TABLE IF EXISTS ten_features_w_result;
-
-CREATE TABLE ten_features_w_result AS
-SELECT
-    t.game_id,
-    t.team_id,
-    t.team_avg,
-    t.team_obp,
-    t.team_slg,
-    t.team_hr,
-    t.team_hitters_k,
-    t.sp_whip,
-    t.sp_baa,
-    t.sp_k,
-    t.sp_hr,
-    r.home_away,
-    r.winner_home_or_away
-FROM ten_feature_stats t
-JOIN game_results r
-ON t.game_id = r.game_id
-AND t.team_id = r.team_id;
-
-
-
+    hb.game_id,
+    hb.H / hb.AB AS home_avg,
+    (hb.H + hb.BB + hb.HBP) / (hb.AB + hb.BB + hb.HBP + hb.SF) AS home_obp,
+    (hb.singles + 2 * hb.doubles + 3 * hb.triples + 4 * hb.HR) / hb.AB AS home_slg,
+    (hp.BB + hp.H) / NULLIF(hp.IP, 0) AS home_starter_whip,
+    hp.H / NULLIF(hp.AB, 0) AS home_starter_baa,
+    hp.K AS home_starter_k,
+    ab.H / ab.AB AS away_avg,
+    (ab.H + ab.BB + ab.HBP) / (ab.AB + ab.BB + ab.HBP + ab.SF) AS away_obp,
+    (ab.singles + 2 * ab.doubles + 3 * ab.triples + 4 * ab.HR) / ab.AB AS away_slg,
+    (ap.BB + ap.H) / NULLIF(ap.IP, 0) AS away_starter_whip,
+    ap.H / NULLIF(ap.AB, 0) AS away_starter_baa,
+    ap.K AS away_starter_k,
+    CASE
+        WHEN bs.winner_home_or_away = 'H'
+            THEN 1
+        ELSE 0
+    END AS HomeTeamWins
+FROM home_team_batter_stats hb
+JOIN away_team_batter_stats ab
+ON hb.game_id = ab.game_id
+JOIN home_starting_pitcher_stats hp
+ON hb.game_id = hp.game_id
+JOIN away_starting_pitcher_stats ap
+ON hb.game_id = ap.game_id
+JOIN boxscore bs
+ON hb.game_id = bs.game_id;
 
 
 

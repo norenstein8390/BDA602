a0893fefd460f4229051da197677e92f8b310f11
Nathan Orenstein <norenstein8390@sdsu.edu>
2022-12-01 14:28:54 -0800

    Homework 6 Update - Thu 12/1 (Test)

--- src/homework6/main_bash.sh
+++ src/homework6/main_bash.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+sleep 10
+
 echo "in bash"
 
-if ! mariadb -u root -ppassword123 -h mariadb-nmo "USE baseball;"
+if ! mariadb -u root -ppassword123 -h mariadb-nmo -e "USE baseball;"
 then
   echo "in loop"
   mariadb -u root -ppassword123 -h mariadb-nmo -e "CREATE DATABASE baseball;"
-  mariadb -u root -ppassword123 -h mariadb-nmo baseball < ./baseball.sql
+  mariadb -u root -ppassword123 -h mariadb-nmo -D baseball < baseball.sql
 fi
 
-mariadb -u root -ppassword123 -h mariadb-nmo baseball < ./homework6.sql
+mariadb -u root -ppassword123 -h mariadb-nmo baseball < homework6.sql
 mariadb -u root -ppassword123 -h mariadb-nmo baseball -e "SELECT * FROM rolling_avg;" > /output/output.txt
